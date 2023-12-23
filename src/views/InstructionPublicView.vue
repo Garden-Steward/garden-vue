be35511f14dc97cf4fad180294ccdfe1e5699a9d
Cameron Preston <cameron@shopvida.com>
2023-12-22 16:40:01 -0800

    Adding ability to approve task and hit endpoint

--- src/views/InstructionPublicView.vue
+++ src/views/InstructionPublicView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
 import { useRoute } from "vue-router";
 import MarkdownIt from 'markdown-it';
 import markdownItAttrs from 'markdown-it-attrs';
@@ -14,12 +15,24 @@ const instSTore = instructionStore();
 const route = useRoute()
 const { instruction } = storeToRefs(instSTore);
 let renderedContent = '';
+const isApproved = ref(false);
+
 
 watch(instruction, (newVal) => {
   if (newVal.attributes) {
     renderedContent = md.render(newVal.attributes?.content);
   }
 });
+
+const acceptTask = (e) => {
+  console.log(e);
+  const urlParams = new URLSearchParams(window.location.search);
+  const user = urlParams.get('u')
+
+  instSTore.approveTask({userId:user, slug:route.params.slug})
+  isApproved.value = true;
+
+}
 
 console.log("instruction: ", instruction);
 
@@ -33,11 +46,13 @@ instSTore.findSlug(route.params.slug);
         <div v-html="renderedContent" class="text-left"></div>
         <!-- Conditional rendering of the agreement button -->
         <div v-if="instruction.attributes?.accept_required" class="mt-6">
-          <button class="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded">
-            Accept
+          <button :class="{ 'bg-gray-500': isApproved, 'bg-green-700 hover:bg-green-900': !isApproved }" class="text-white font-bold py-2 px-4 rounded" @click="acceptTask" :disabled="isApproved">
+            {{ isApproved ? 'Accepted' : 'Accept' }}
           </button>
           <p class="text-sm mt-2">
-            I understand this task's requirements and I am capable. I accept this task.
+            {{ isApproved ? 
+              'Thank you for accepting! You have now unlocked the ability to be assigned this task in the future.'
+              : 'I understand this task\'s requirements and I am capable. I accept this task.' }}
           </p>
         </div>
       </div>
