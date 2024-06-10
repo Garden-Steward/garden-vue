f573b7854b49cab8513fb7b6469b58c0bef7a63d
Cameron Preston <cameron@shopvida.com>
2024-06-09 20:41:49 -0700

    adding loading spinner to blogs

--- src/views/BlogList.vue
+++ src/views/BlogList.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
 
 import { useBlogStore } from '@/stores';
 
@@ -7,7 +8,12 @@ const blogStore = useBlogStore();
 const baseUrl = `${import.meta.env.VITE_API_URL}`;
 
 const { blogs } = storeToRefs(blogStore);
-blogStore.fetchAll();
+const isLoading = ref(true);  // Add this line
+
+blogStore.fetchAll().then(() => {
+  console.log('got blogs: ', blogs)
+  isLoading.value = false;  // Set loading to false once data is fetched
+});
 
 let heroImage = function(blog) {
   if (import.meta.env.VITE_API_URL == 'http://localhost:1337') {
@@ -25,7 +31,12 @@ let heroImage = function(blog) {
         <h1 class="text-3xl font-bold mb-4 font-roboto">Welcome to the Garden Steward Blog</h1>
         <p class="text-md mb-4 font-roboto">Hi there, I'm Cameron and I'm the founder of Garden Steward, and co-founder and catalyst of the Oakland Urban Farming Project. I'm on a mission to create abundance through urban gardens. I'm in the process of writing a book to help more people understand the impact having publically accessible gardens can have on people's lives. I'm also developing an app to help manage the garden projects so anyone, anywhere will easily be able to contribute and manage gardens in their community.</p>
         <h2 class="text-xl mb-4 font-roboto">Latest Blog Posts</h2>
-        <div class="space-y-8"> <!-- Increased vertical spacing between blog posts -->
+        <!-- Loading spinner -->
+        <div v-if="isLoading" class="flex justify-center items-center">
+          <span class="loader"></span> <!-- Add your spinner element here -->
+        </div>
+        <!-- Blog posts -->
+        <div v-else class="space-y-8"> <!-- Increased vertical spacing between blog posts -->
           <div v-for="blog in blogs" :key="blog.id" class="flex flex-col md:flex-row mx-auto bg-custom-light rounded-lg shadow-md overflow-hidden md:max-w-4xl border-white border hover:bg-custom-lighter"> <!-- Adjusted hover opacity -->
             <a :href="`/blog/${blog.attributes.slug}`" class="md:flex no-underline hover:no-underline"> <!-- Ensured no underlining on hover -->
               <img class="h-64 w-full lg:w-64 md:w-48 object-cover" :src="heroImage(blog)" :alt="blog.attributes.title"> <!-- Increased height of the image -->
@@ -49,4 +60,17 @@ let heroImage = function(blog) {
 
 <style>
 /* Additional styles if needed */
+.loader {
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #3498db;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
 </style>
