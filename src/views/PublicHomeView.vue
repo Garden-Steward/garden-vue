e1e36445ba1cdc422d0f53eba6705c6919dea746
Rowan <rowan@steward.garden>
2026-09-10 16:49:48 -0700

    copy: steward subline → 'In a mission to restore biodiversity.'

--- src/views/PublicHomeView.vue
+++ src/views/PublicHomeView.vue
@@ -19,7 +19,7 @@ const videoSections = [
   {
     id: 'steward',
     line: 'We are its steward.',
-        subline: 'Garden Steward helps communities organize around their land.',
+        subline: 'In a mission to restore biodiversity.',
     overlay: 'rgba(10, 20, 15, 0.5)',
     dark: true,
     src: null,
